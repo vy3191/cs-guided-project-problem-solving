e93325c6ac3c91b3bab88d4ff1ffa3d128ff2ea0
vy3191 <venkyafhs@gmail.com>
2020-12-03 21:01:37 -0500

    Completed assingment#3

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -16,4 +16,13 @@ Notes:
 
 def multiply_nums(nums):
     # Your code here
-
+    new_nums = nums.split(", ")
+    result = 1
+    for num in new_nums:
+        result *= int(num)
+    print(result)
+    return result
+multiply_nums("2, 3") 
+multiply_nums("1, 2, 3, 4") 
+multiply_nums("54, 75, 453, 0") 
+multiply_nums("10, -2") 
